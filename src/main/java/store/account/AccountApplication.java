1e1fa014770f40d121211eeb1c5fdbd423fdc9a7
Humberto Sandmann <hsandmann@ieee.org>
2025-08-29 12:58:20 -0300

    dockerfile

--- src/main/java/store/account/AccountApplication.java
+++ src/main/java/store/account/AccountApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AccountServiceApplication {
+public class AccountApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AccountServiceApplication.class, args);
+		SpringApplication.run(AccountApplication.class, args);
 	}
 
 }
